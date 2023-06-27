8e747208493b094a14d9c09c527f7e75a66fd937
bknote71 <bknote71@gmail.com>
2023-06-27 21:10:09 +0900

    branch init

--- filesys/inode.c
+++ filesys/inode.c
@@ -31,12 +31,14 @@ bytes_to_sectors(off_t size)
 /* In-memory inode. */
 struct inode
 {
-    struct list_elem elem;  /* Element in inode list. */
-    disk_sector_t sector;   /* Sector number of disk location. */
-    int open_cnt;           /* Number of openers. */
-    bool removed;           /* True if deleted, false otherwise. */
-    int deny_write_cnt;     /* 0: writes ok, >0: deny writes. */
-    struct inode_disk data; /* Inode content. */
+    struct list_elem elem; /* Element in inode list. */
+    disk_sector_t sector;  /* Sector number of disk location. */
+    int open_cnt;          /* Number of openers. */
+    bool removed;          /* True if deleted, false otherwise. */
+    int deny_write_cnt;    /* 0: writes ok, >0: deny writes. */
+
+    off_t length;
+    // struct inode_disk data;             /* Inode content. */
 };
 
 /* Returns the disk sector that contains byte offset POS within
